044596b486e1ea283448f706e3a92bde225aedcd
Yash Saxena <yashsaxena2111@gmail.com>
2022-06-23 23:50:00 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")
+message = """Subject: "Model created Success full"
+Project is done."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
+
+# In[ ]:
+
+
+
+
